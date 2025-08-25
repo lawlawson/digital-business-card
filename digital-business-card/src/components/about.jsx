0997d21d2de159e03901a5f16efaee0dacb0efd7
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2025-08-25 23:19:17 +0100

    added about component

---
+++ digital-business-card/src/components/about.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="about">
+      <h2>About</h2>
+      <p>
+        I am a passionate web developer with experience in building responsive and interactive web applications. I enjoy learning new technologies and solving challenging problems. My focus is on creating user-friendly digital experiences.
+      </p>
+    </section>
+  );
+}
