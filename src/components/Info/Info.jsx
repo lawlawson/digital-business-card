17f3bef56b75c5fde8de32ba244f152b70e96081
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2025-10-19 22:08:07 +0100

    updated typography on about section an title

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -5,8 +5,8 @@ function Info() {
     <div className='info'>
       <img src='./src/assets/profile-pic.jpg' alt='profile-pic' />
       <h2>Lawrence Narh-Lawson</h2>
-      <p>Front End Developer</p>
-      <p>lawlawson.dev</p>
+      <p className='job-title'>Front End Developer</p>
+      <p className='website'>lawlawson.dev</p>
       <div className='buttons'>
         <button className='email-button'>Email</button>
         <button className='linkedin-button'>LinkedIn</button>
